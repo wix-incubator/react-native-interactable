025032a9a01abfd10b9a049fb157c2f8dba058bc
talkol <talkol@wix.com>
2017-01-28 01:49:53 +0200

    native events aren't working yet

--- src/InteractableView.js
+++ src/InteractableView.js
@@ -3,6 +3,18 @@ import { requireNativeComponent, Animated } from 'react-native';
 
 const NativeInteractableView = requireNativeComponent('InteractableView', null);
 
+/*
+// is this required in order to support native events?
+const NativeInteractableView = Animated.createAnimatedComponent(requireNativeComponent('InteractableView', null));
+
+// this is a good test to check if our events are native or not
+setInterval(() => {
+  for (let i=0; i<1e9; i++) {
+    let j = 4;
+  }
+}, 1000);
+*/
+
 export default class InteractableView extends Component {
   constructor(props) {
     super(props);

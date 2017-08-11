eaa9a12250689cba3033f9f3949cc30f432767fe
Alberto Schiabel <jkomyno@users.noreply.github.com>
2017-08-11 14:47:32 +0200

    Update Interactable.java
    Removed createJSModules override to avoid build error on react-native@^0.47.0

--- lib/android/src/main/java/com/wix/interactable/Interactable.java
+++ lib/android/src/main/java/com/wix/interactable/Interactable.java
@@ -17,11 +17,6 @@ public class Interactable implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.<ViewManager>singletonList(new InteractableViewManager());
     }

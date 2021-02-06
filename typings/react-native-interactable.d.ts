17168843541ee323f96287354ad8dc6c6fd35ea5
David <4661784+retyui@users.noreply.github.com>
2021-02-06 23:47:30 +0200

    Update .d.ts
    Issue: https://github.com/wix/react-native-interactable/issues/318

--- typings/react-native-interactable.d.ts
+++ typings/react-native-interactable.d.ts
@@ -1,144 +1,147 @@
 declare module 'react-native-interactable' {
+  import { Animated, ViewProperties, ViewStyle } from 'react-native';
 
-  import {Animated, ViewProperties, ViewStyle} from 'react-native';
-
-  namespace Interactable {
-    interface ISnapPoint {
-      x?: number;
-      y?: number;
-      damping?: number;
-      tension?: number;
-      id?: string;
-    }
-
-    interface IInfluenceArea {
-      left?: number;
-      right?: number;
-      top?: number;
-      bottom?: number;
-    }
-
-    interface ISpringPoint {
-      x?: number;
-      y?: number;
-      damping?: number;
-      tension?: number;
-      influenceArea?: IInfluenceArea;
-      haptics?: boolean;
-    }
-
-    interface IGravityPoints {
-      x?: number;
-      y?: number;
-      strength?: number;
-      falloff?: number;
-      damping?: number;
-      influenceArea?: IInfluenceArea;
-      haptics?: boolean;
-    }
-
-    interface IFrictionArea {
-      damping?: number;
-      influenceArea?: IInfluenceArea;
-      haptics?: boolean;
-    }
-
-    interface IAlertArea {
-      id: string;
-      influenceArea: IInfluenceArea;
-    }
-
-    interface IBoundaries {
-      left?: number;
-      right?: number;
-      top?: number;
-      bottom?: number;
-      bounce?: number;
-      haptics?: boolean;
-    }
-
-    interface IDragWithSpring {
-      tension?: number;
-      damping?: number;
-    }
-
-    interface IInitialPosition {
-      x?: number;
-      y?: number;
-    }
-
-    interface INativeSnapEvent {
-      index: number;
-      id: string;
-    }
-
-    interface ISnapEvent {
-      nativeEvent: INativeSnapEvent;
-    }
-
-    interface INativeStopEvent {
-      x: number;
-      y: number;
-    }
-
-    interface IStopEvent {
-      nativeEvent: INativeStopEvent;
-    }
-
-    type NativeDragEventStartStateType = 'start';
-    type NativeDragEventEndStateType = 'end';
-    type NativeDragEventState = NativeDragEventStartStateType | NativeDragEventEndStateType;
-
-    interface INativeDragEvent {
-      state: NativeDragEventState;
-      x: number;
-      y: number;
-    }
-
-    interface IDragEvent {
-      nativeEvent: INativeDragEvent;
-    }
-
-    type NativeAlertEventEnterValueType = 'enter';
-    type NativeAlertEventLeaveValueType = 'leave';
-    type NativeAlertEventValue = NativeAlertEventEnterValueType | NativeAlertEventLeaveValueType;
-
-    interface INativeAlertEvent { [id: string]: NativeAlertEventValue }
-
-    interface IAlertEvent {
-      nativeEvent: INativeAlertEvent;
-    }
-
-    interface IInteractableView extends ViewProperties {
-      snapPoints?: ISnapPoint[];
-      springPoints?: ISpringPoint[];
-      gravityPoints?: IGravityPoints[];
-      frictionAreas?: IFrictionArea[];
-      alertAreas?: IAlertArea[];
-      horizontalOnly?: boolean;
-      verticalOnly?: boolean;
-      boundaries?: IBoundaries;
-      onSnap?: (event: ISnapEvent) => void;
-      onSnapStart?: (event: ISnapEvent) => void;
-      onStop?: (event: IStopEvent) => void;
-      onDrag?: (event: IDragEvent) => void;
-      onAlert?: (event: IAlertEvent) => void;
-      dragEnabled?: boolean;
-      dragWithSpring?: IDragWithSpring;
-      dragToss?: number;
-      animatedValueX?: Animated.Value;
-      animatedValueY?: Animated.Value;
-      animatedNativeDriver?: boolean;
-      initialPosition?: IInitialPosition;
-      style?: ViewStyle;
-    }
-
-    interface IInteractable {
-      View: new () => React.Component<IInteractableView, {}>;
-    }
+  export interface ISnapPoint {
+    x?: number;
+    y?: number;
+    damping?: number;
+    tension?: number;
+    id?: string;
   }
 
-  const Interactable: Interactable.IInteractable;
+  export interface IInfluenceArea {
+    left?: number;
+    right?: number;
+    top?: number;
+    bottom?: number;
+  }
 
-  export = Interactable;
+  export interface ISpringPoint {
+    x?: number;
+    y?: number;
+    damping?: number;
+    tension?: number;
+    influenceArea?: IInfluenceArea;
+    haptics?: boolean;
+  }
+
+  export interface IGravityPoints {
+    x?: number;
+    y?: number;
+    strength?: number;
+    falloff?: number;
+    damping?: number;
+    influenceArea?: IInfluenceArea;
+    haptics?: boolean;
+  }
+
+  interface IFrictionArea {
+    damping?: number;
+    influenceArea?: IInfluenceArea;
+    haptics?: boolean;
+  }
+
+  export interface IAlertArea {
+    id: string;
+    influenceArea: IInfluenceArea;
+  }
+
+  export interface IBoundaries {
+    left?: number;
+    right?: number;
+    top?: number;
+    bottom?: number;
+    bounce?: number;
+    haptics?: boolean;
+  }
+
+  export interface IDragWithSpring {
+    tension?: number;
+    damping?: number;
+  }
+
+  export interface IInitialPosition {
+    x?: number;
+    y?: number;
+  }
+
+  export interface INativeSnapEvent {
+    index: number;
+    id: string;
+  }
+
+  export interface ISnapEvent {
+    nativeEvent: INativeSnapEvent;
+  }
+
+  export interface INativeStopEvent {
+    x: number;
+    y: number;
+  }
+
+  export interface IStopEvent {
+    nativeEvent: INativeStopEvent;
+  }
+
+  export type NativeDragEventStartStateType = 'start';
+  export type NativeDragEventEndStateType = 'end';
+  export type NativeDragEventState =
+    | NativeDragEventStartStateType
+    | NativeDragEventEndStateType;
+
+  export interface INativeDragEvent {
+    state: NativeDragEventState;
+    x: number;
+    y: number;
+  }
+
+  export interface IDragEvent {
+    nativeEvent: INativeDragEvent;
+  }
+
+  export type NativeAlertEventEnterValueType = 'enter';
+  export type NativeAlertEventLeaveValueType = 'leave';
+  export type NativeAlertEventValue =
+    | NativeAlertEventEnterValueType
+    | NativeAlertEventLeaveValueType;
+
+  export interface INativeAlertEvent {
+    [id: string]: NativeAlertEventValue;
+  }
+
+  export interface IAlertEvent {
+    nativeEvent: INativeAlertEvent;
+  }
+
+  export interface IInteractableView extends ViewProperties {
+    snapPoints?: ISnapPoint[];
+    springPoints?: ISpringPoint[];
+    gravityPoints?: IGravityPoints[];
+    frictionAreas?: IFrictionArea[];
+    alertAreas?: IAlertArea[];
+    horizontalOnly?: boolean;
+    verticalOnly?: boolean;
+    boundaries?: IBoundaries;
+    onSnap?: (event: ISnapEvent) => void;
+    onSnapStart?: (event: ISnapEvent) => void;
+    onStop?: (event: IStopEvent) => void;
+    onDrag?: (event: IDragEvent) => void;
+    onAlert?: (event: IAlertEvent) => void;
+    dragEnabled?: boolean;
+    dragWithSpring?: IDragWithSpring;
+    dragToss?: number;
+    animatedValueX?: Animated.Value;
+    animatedValueY?: Animated.Value;
+    animatedNativeDriver?: boolean;
+    initialPosition?: IInitialPosition;
+    style?: ViewStyle;
+  }
+
+  export interface IInteractable {
+    View: new () => React.Component<IInteractableView, {}>;
+  }
+
+  const Interactable: IInteractable;
+
+  export default Interactable;
 }
